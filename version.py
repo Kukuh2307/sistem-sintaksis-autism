54fb877c1545f630ea190659dc964ccc0622ec59
Kisawa16 <kukuhagung12@gmail.com>
2026-06-27 21:19:57 +0700

    (finish v1.1) menambahkan versi

---
+++ version.py
@@ -0,0 +1,9 @@
+import sklearn
+import joblib
+import pandas
+import streamlit
+
+print(f"Scikit-learn: {sklearn.__version__}")
+print(f"Joblib: {joblib.__version__}")
+print(f"Pandas: {pandas.__version__}")
+print(f"Streamlit: {streamlit.__version__}")
